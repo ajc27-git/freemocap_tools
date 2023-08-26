e48ea5661b67aa376a1dd599b3c69ec7ec5acc1e
jonmatthis <jonmatthis@gmail.com>
2023-08-26 13:11:12 -0400

    starting refactor

---
+++ freemocap_adapter/__main__.py
@@ -0,0 +1,10 @@
+
+
+
+adjust_empties(z_align_ref_empty=fmc_adapter_tool.vertical_align_reference,
+               z_align_angle_offset=fmc_adapter_tool.vertical_align_angle_offset,
+               ground_ref_empty=fmc_adapter_tool.ground_align_reference,
+               z_translation_offset=fmc_adapter_tool.vertical_align_position_offset,
+               correct_fingers_empties=fmc_adapter_tool.correct_fingers_empties,
+               add_hand_middle_empty=fmc_adapter_tool.add_hand_middle_empty
+               )
