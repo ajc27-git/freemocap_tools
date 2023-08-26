d66490c45b38b3f24aea5d2006b4519e99b02c81
jonmatthis <jonmatthis@gmail.com>
2023-08-26 17:24:35 -0400

    registers just fine when running in blender/scripting

--- freemocap_adapter/__init__.py
+++ freemocap_adapter/__init__.py
@@ -1,6 +1,9 @@
 import logging
 
-from freemocap_adapter.user_interface.setup.register import register
+import bpy
+
+from freemocap_adapter.user_interface import USER_INTERFACE_CLASSES, FMC_ADAPTER_PROPERTIES
+
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +14,7 @@ logger = logging.getLogger(__name__)
 ### The rig has a TPose as rest pose for easier retargeting.
 ### For best results, when the script is ran the empties should be
 ### forming a standing still pose with arms open similar to A or T Pose
+
 ### The body_mesh.ply file should be in the same folder as the
 ### Blender file before manually opening it.
 #######################################################################
@@ -25,6 +29,30 @@ bl_info = {
     'category': 'Development',
 }
 
+
+def unregister():
+    logger.info(f"Unregistering {__file__} as add-on")
+    for cls in USER_INTERFACE_CLASSES:
+        logger.info(f"Unregistering class {cls.__name__}")
+        bpy.utils.unregister_class(cls)
+
+    logger.info(f"Unregistering property group FMC_ADAPTER_PROPERTIES")
+    del bpy.types.Scene.fmc_adapter_tool
+
+
+def register():
+    logger.info(f"Registering {__file__} as add-on")
+    for cls in USER_INTERFACE_CLASSES:
+        logger.info(f"Registering class {cls.__name__}")
+        bpy.utils.register_class(cls)
+
+    logger.info(f"Registering property group FMC_ADAPTER_PROPERTIES")
+    bpy.types.Scene.fmc_adapter_tool = bpy.props.PointerProperty(type=FMC_ADAPTER_PROPERTIES)
+
+    logger.info(f"Finished registering {__file__} as add-on!")
+
+
 if __name__ == "__main__":
+    print("HHHHEEEEE")
     logger.info(f"Running {__file__} as main file ")
     register()
