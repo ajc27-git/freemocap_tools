75373c02dccfaa24fb9de0d6d0e918caaf1b6d31
jonmatthis <jonmatthis@gmail.com>
2024-05-15 16:02:48 -0400

    Bump version v2023.10.1017 -> v2024.05.1018

--- ajc27_freemocap_blender_addon/__init__.py
+++ ajc27_freemocap_blender_addon/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Skelly FreeMoCap"""
 __email__ = "info@freemocap.org"
-__version__ = "v2023.10.1017"
+__version__ = "v2024.05.1018"
 
 #######################################################################
 ### Add-on to adapt the Freemocap Blender output. It can adjust the
